47062ad243c269e50ef24588cf9e580779c9d23f
Dan Burkert <dan@danburkert.com>
2014-07-12 13:17:17 -0700

    version 1 - not working

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![crate_type = "lib"]
-#![crate_id = "sort#0.1"]
-
-extern crate debug;
-extern crate collections;
-
-extern crate quickcheck;
-
-pub mod patience;
--- src/patience-sort.rs
+++ src/patience-sort.rs
@@ -1,3 +1,11 @@
+#![crate_type = "lib"]
+#![crate_id = "sort#0.1"]
+
+extern crate debug;
+extern crate collections;
+
+extern crate quickcheck;
+
 #[test]
 extern crate quickcheck;
 #[test]
